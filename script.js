48eff10f63342ce543aacea5513c7fd7bec14dab
ninja1509 <63071772+ninja1509@users.noreply.github.com>
2022-09-15 15:47:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
 
 // window.setInterval('refresh()', 10000); 	
 
-async function TimeAPI(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
-    var responseJSON = await response.json();
 
-    var datetime = responseJSON.datetime;
-    var hour = datetime.slice(11,13);
-    var minutes = datetime.slice(14,16);
-    
-    console.log(hour);
-    console.log(minutes)
-
-
-    document.getElementById("hours").innerHTML=hour;
-    document.getElementById("minutes").innerHTML=minutes;
-    
-    // window .location.reload();
-
-}
 
    
  
