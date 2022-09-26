e1d9737ff33909aec67d62b1c94865148fbb8592
ninja1509 <63071772+ninja1509@users.noreply.github.com>
2022-09-26 18:34:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+async function TimeAPI(){
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
 
-// window.setInterval('refresh()', 10000); 	
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    var minutes = datetime.slice(14,16);
+    
+    console.log(hour);
+    console.log(minutes)
 
+    document.getElementById("hours").innerHTML=hour;
+    document.getElementById("minutes").innerHTML=minutes;
 
+}
 
    
  
